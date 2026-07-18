c4401d3aeb5d58f3dc3464bcbcde01056449e359
Aathi <aathithirunav@gmail.com>
2026-07-18 19:09:39 +0530

    feat(repository): Add ProductRepository
    - Create a new repository interface for Product entity
    - Extend JpaRepository for basic CRUD operations

---
+++ ecommerce-backend/src/main/java/com/app/ecommerce/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.app.ecommerce.repository;
+
+import com.app.ecommerce.models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
