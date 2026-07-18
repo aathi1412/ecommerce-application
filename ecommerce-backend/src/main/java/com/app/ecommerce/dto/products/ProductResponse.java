d0d26ff2153dde9e88e686d260314afbedcb8c21
Aathi <aathithirunav@gmail.com>
2026-07-18 19:10:11 +0530

    feat(products): Add ProductResponse
    - Introduced a new ProductResponse data transfer object to handle product responses

---
+++ ecommerce-backend/src/main/java/com/app/ecommerce/dto/products/ProductResponse.java
@@ -0,0 +1,18 @@
+package com.app.ecommerce.dto.products;
+
+import lombok.Builder;
+
+import java.math.BigDecimal;
+
+@Builder
+public record ProductResponse(
+        Long id,
+        String name,
+        String description,
+        BigDecimal price,
+        Integer stockQuantity,
+        String category,
+        String imageUrl,
+        Boolean active
+) {
+}
