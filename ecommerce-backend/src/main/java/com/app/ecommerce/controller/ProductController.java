0d9f358bb3e975466d6d878290787e2cf8786c15
Aathi <aathithirunav@gmail.com>
2026-07-18 21:37:19 +0530

    refactor(controller): Simplify annotations
    - Removed redundant import statements
    - Added @RequestBody annotation to addProduct method

--- ecommerce-backend/src/main/java/com/app/ecommerce/controller/ProductController.java
+++ ecommerce-backend/src/main/java/com/app/ecommerce/controller/ProductController.java
@@ -7,10 +7,7 @@ import com.app.ecommerce.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,11 +24,13 @@ public class ProductController {
     }
 
     @PostMapping("/add")
-    public ResponseEntity<ProductResponse> addProduct(ProductRequest request){
+    public ResponseEntity<ProductResponse> addProduct(@RequestBody ProductRequest request){
         ProductResponse response = productService.createProduct(request);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(response);
     }
+
+
 
 }
