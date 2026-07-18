230e51350729ee25d5243929fab5a189b14af307
Aathi <aathithirunav@gmail.com>
2026-07-18 19:22:21 +0530

    feat(products): Add Get All Products Endpoint
    - Added a new GET endpoint to retrieve all products
    - Implemented the getAllProducts method in ProductController

--- ecommerce-backend/src/main/java/com/app/ecommerce/controller/ProductController.java
+++ ecommerce-backend/src/main/java/com/app/ecommerce/controller/ProductController.java
@@ -2,13 +2,17 @@ package com.app.ecommerce.controller;
 
 import com.app.ecommerce.dto.products.ProductRequest;
 import com.app.ecommerce.dto.products.ProductResponse;
+import com.app.ecommerce.models.Product;
 import com.app.ecommerce.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -16,6 +20,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
 
     private final ProductService productService;
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts(){
+        return ResponseEntity.ok().build();
+    }
 
     @PostMapping("/add")
     public ResponseEntity<ProductResponse> addProduct(ProductRequest request){
