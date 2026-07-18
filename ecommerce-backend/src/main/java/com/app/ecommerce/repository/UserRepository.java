f1ca673cd1b4f35439b8eafd77014a0d3bb8f766
Aathi <aathithirunav@gmail.com>
2026-07-18 19:20:10 +0530

    feat(repository): Add UserRepository
    - Create UserRepository interface to handle user data access
    - Extend JpaRepository for basic CRUD operations
    - Add custom method to find user by email

---
+++ ecommerce-backend/src/main/java/com/app/ecommerce/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.app.ecommerce.repository;
+
+import com.app.ecommerce.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
