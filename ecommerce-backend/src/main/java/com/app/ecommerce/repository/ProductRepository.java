379124de6b125bf2695148d861c4f07a79027a6c
Aathi <aathithirunav@gmail.com>
2026-07-19 16:24:24 +0530

    feat(productrepository): Add product search functionality
    - Implement findByActiveTrue method to retrieve active products
    - Add searchProducts method to find products by keyword

--- ecommerce-backend/src/main/java/com/app/ecommerce/repository/ProductRepository.java
+++ ecommerce-backend/src/main/java/com/app/ecommerce/repository/ProductRepository.java
@@ -2,8 +2,22 @@ package com.app.ecommerce.repository;
 
 import com.app.ecommerce.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<Product> findByActiveTrue();
+
+    @Query("SELECT p FROM products p " +
+            "WHERE p.active=true AND " +
+            "p.stockQuantity > 0 AND " +
+            "LOWER(p.name) LIKE LOWER(concat('%', :keyword, '%'))"
+            )
+    List<Product> searchProducts(@Param("keyword") String keyword);
 }
