5de4911a965a590d5b7366d6e2df4269208d6483
Aathi <aathithirunav@gmail.com>
2026-07-19 16:24:17 +0530

    feat(exceptions): Add ProductNotFoundException
    - Create a custom exception for product not found scenarios

---
+++ ecommerce-backend/src/main/java/com/app/ecommerce/exceptions/ProductNotFoundException.java
@@ -0,0 +1,7 @@
+package com.app.ecommerce.exceptions;
+
+public class ProductNotFoundException extends RuntimeException {
+    public ProductNotFoundException(String message) {
+        super(message);
+    }
+}
