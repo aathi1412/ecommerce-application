40c300c8524d5980a6814b4fc921dafdd01cc44d
Aathi <aathithirunav@gmail.com>
2026-07-19 21:30:25 +0530

    refactor(cartitem): Apply Lombok Annotations
    - Added @Builder annotation for easier object creation
    - Added @NoArgsConstructor and @AllArgsConstructor for improved constructor handling

--- ecommerce-backend/src/main/java/com/app/ecommerce/models/CartItem.java
+++ ecommerce-backend/src/main/java/com/app/ecommerce/models/CartItem.java
@@ -1,15 +1,21 @@
 package com.app.ecommerce.models;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
 import java.time.Instant;
 
+@Builder
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CartItem {
 
     @Id
