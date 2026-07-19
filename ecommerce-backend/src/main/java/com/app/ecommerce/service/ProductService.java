f4f3ba5559767e9a1cf89f678550b0acaaa1c980
Aathi <aathithirunav@gmail.com>
2026-07-19 16:24:34 +0530

    feat(productservice): Implement product management
    - Added methods to retrieve all products, active products, and a product by id
    - Implemented product creation, update, and deletion functionality
    - Added search functionality for products

--- ecommerce-backend/src/main/java/com/app/ecommerce/service/ProductService.java
+++ ecommerce-backend/src/main/java/com/app/ecommerce/service/ProductService.java
@@ -1,25 +1,91 @@
 package com.app.ecommerce.service;
 
+import com.app.ecommerce.dto.ApiResponse;
 import com.app.ecommerce.dto.products.ProductRequest;
 import com.app.ecommerce.dto.products.ProductResponse;
+import com.app.ecommerce.exceptions.ProductNotFoundException;
+import com.app.ecommerce.exceptions.UserNotFoundException;
 import com.app.ecommerce.models.Product;
 import com.app.ecommerce.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Instant;
+import java.util.List;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ProductService{
 
     private final ProductRepository productRepository;
 
-    public ProductResponse createProduct(ProductRequest productRequest){
-        Product  product = mapRequestToProduct(productRequest);
+    public List<ProductResponse> getAllProducts(){
+        return productRepository.findAll().stream()
+                .map(this::mapProductToResponse)
+                .toList();
+    }
+
+    public List<ProductResponse> getActiveProducts(){
+        return productRepository.findByActiveTrue().stream()
+                .map(this::mapProductToResponse)
+                .toList();
+    }
+
+    public ProductResponse getProductById(Long id){
+        return productRepository.findById(id)
+                .map(this::mapProductToResponse)
+                .orElseThrow(() -> {
+                    log.error("Product not found with id {}", id);
+                    return new ProductNotFoundException("Product Not Found");
+                });
+
+    }
+
+    public ProductResponse createProduct(ProductRequest request){
+        log.info("Creating product: name='{}', category='{}'",
+                request.name(), request.category());
+
+        Product  product = mapRequestToProduct(request);
+
         Product savedProduct = productRepository.save(product);
-        System.out.println(savedProduct);
+        log.info("Product created successfully with id={}", savedProduct.getId());
+
         return mapProductToResponse(savedProduct);
     }
 
+    @Transactional
+    public ProductResponse updateProduct(Long id, ProductRequest request) {
+        return productRepository.findById(id)
+                .map(existingProduct -> {
+                    updateProductFromRequest(existingProduct, request);
+                    log.info("Product updated successfully with id={}", existingProduct.getId());
+                    return mapProductToResponse(existingProduct);
+                })
+                .orElseThrow(() -> {
+                    log.warn("Product not found with id={}", id);
+                    return new UserNotFoundException("Product id: " + id + "not found");
+                });
+    }
+
+    public ApiResponse deleteProductById(Long id) {
+        productRepository.deleteById(id);
+
+        return ApiResponse.builder()
+                .timeStamp(Instant.now())
+                .status(HttpStatus.OK.value())
+                .message("Product Deleted Successfully")
+                .build();
+    }
+
+    public List<ProductResponse> searchProducts(String keyword){
+        return productRepository.searchProducts(keyword).stream()
+                .map(this::mapProductToResponse)
+                .toList();
+    }
     private ProductResponse mapProductToResponse(Product product){
         return ProductResponse.builder()
                 .id(product.getId())
@@ -32,6 +98,7 @@ public class ProductService{
                 .active(product.getActive())
                 .build();
     }
+
     private Product mapRequestToProduct(ProductRequest request){
         return Product.builder()
                 .name(request.name())
@@ -42,4 +109,15 @@ public class ProductService{
                 .imageUrl(request.imageUrl())
                 .build();
     }
+
+    private void updateProductFromRequest(Product product, ProductRequest request) {
+        product.setName(request.name());
+        product.setDescription(request.description());
+        product.setPrice(request.price());
+        product.setStockQuantity(request.stockQuantity());
+        product.setCategory(request.category());
+        product.setImageUrl(request.imageUrl());
+    }
+
+
 }
