3859741d0adff179578b750d67720e1fced7bac6
Aathi <aathithirunav@gmail.com>
2026-07-19 21:32:57 +0530

    refactor(exceptions): Simplify exception handling
    - Extracted error response building into a separate method
    - Improved readability of exception handlers

--- ecommerce-backend/src/main/java/com/app/ecommerce/exceptions/GlobalExceptionHandler.java
+++ ecommerce-backend/src/main/java/com/app/ecommerce/exceptions/GlobalExceptionHandler.java
@@ -13,32 +13,77 @@ import java.time.Instant;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleUserNotFoundException(UserNotFoundException ex, HttpServletRequest request) {
-        ErrorResponse errorResponse = ErrorResponse.builder()
-                            .timeStamp(Instant.now())
-                            .status(HttpStatus.NOT_FOUND.value())
-                            .error(HttpStatus.NOT_FOUND.getReasonPhrase())
-                            .message(ex.getMessage())
-                            .path(request.getRequestURI())
-                            .build();
-
+    public ResponseEntity<ErrorResponse> handleUserNotFoundException(UserNotFoundException ex,
+                                                                     HttpServletRequest request) {
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
-                .body(errorResponse);
+                .body(buildErrorResponse(
+                                ex.getMessage(),
+                                HttpStatus.NOT_FOUND,
+                                request
+                        )
+                );
     }
 
     @ExceptionHandler(ProductNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleProductNotFoundException(ProductNotFoundException ex, HttpServletRequest request) {
-        ErrorResponse errorResponse = ErrorResponse.builder()
-                .timeStamp(Instant.now())
-                .status(HttpStatus.NOT_FOUND.value())
-                .error(HttpStatus.NOT_FOUND.getReasonPhrase())
-                .message(ex.getMessage())
-                .path(request.getRequestURI())
-                .build();
-
+    public ResponseEntity<ErrorResponse> handleProductNotFoundException(ProductNotFoundException ex,
+                                                                        HttpServletRequest request) {
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
-                .body(errorResponse);
+                .body(buildErrorResponse(
+                            ex.getMessage(),
+                            HttpStatus.NOT_FOUND,
+                            request
+                        )
+                );
+    }
+
+    @ExceptionHandler(CartItemNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleCartItemNotFoundException(CartItemNotFoundException ex,
+                                                                        HttpServletRequest request) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(buildErrorResponse(
+                                ex.getMessage(),
+                                HttpStatus.NOT_FOUND,
+                                request
+                        )
+                );
+    }
+
+    @ExceptionHandler(OutOfStockException.class)
+    public ResponseEntity<ErrorResponse> handleOutOfStockException(OutOfStockException ex,
+                                                                        HttpServletRequest request) {
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(buildErrorResponse(
+                                ex.getMessage(),
+                                HttpStatus.CONFLICT,
+                                request
+                        )
+                );
+    }
+
+    @ExceptionHandler(InsufficientStockException.class)
+    public ResponseEntity<ErrorResponse> handleInsufficientStockException(InsufficientStockException ex,
+                                                                   HttpServletRequest request) {
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(buildErrorResponse(
+                                ex.getMessage(),
+                                HttpStatus.CONFLICT,
+                                request
+                        )
+                );
+    }
+
+    public ErrorResponse buildErrorResponse(String exception, HttpStatus status, HttpServletRequest request) {
+        return ErrorResponse.builder()
+                .timeStamp(Instant.now())
+                .status(status.value())
+                .error(status.getReasonPhrase())
+                .message(exception)
+                .path(request.getRequestURI())
+                .build();
     }
 }
