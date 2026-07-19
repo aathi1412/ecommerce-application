1a1458cbc09680f690de72ccee0396997528b084
Aathi <aathithirunav@gmail.com>
2026-07-19 16:23:36 +0530

    feat(model): Add CartItem entity
    - Define CartItem as a JPA entity with id, product, user, quantity, price, createdAt, and updatedAt fields

---
+++ ecommerce-backend/src/main/java/com/app/ecommerce/models/CartItem.java
@@ -0,0 +1,37 @@
+package com.app.ecommerce.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+@Entity
+@Data
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    private Integer quantity;
+
+    private BigDecimal price;
+
+    @CreationTimestamp
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    private Instant updatedAt;
+
+}
