fbddeedcbc03785a414cf1ffcb06974d90b61939
Aathi <aathithirunav@gmail.com>
2026-07-20 12:50:57 +0530

    feat(ordermodel): Add Order model
    - Define Order entity with id, user, total amount, and order status
    - Establish relationships with User and OrderItem entities

---
+++ ecommerce-backend/src/main/java/com/app/ecommerce/models/Order.java
@@ -0,0 +1,47 @@
+package com.app.ecommerce.models;
+
+import com.app.ecommerce.enums.OrderStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+@Builder
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id",  nullable = false)
+    private User user;
+
+    private BigDecimal totalAmount;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    @CreationTimestamp
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    private Instant updatedAt;
+
+
+}
