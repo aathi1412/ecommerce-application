9e6f73a12bc4127fd0bfec1da5a644ec73bd9e41
Aathi <aathithirunav@gmail.com>
2026-07-20 23:21:20 +0530

    feat(orderitem): Add OrderItem model
    - Create a new entity to represent order items
    - Include fields for product, order, quantity, and price

---
+++ ecommerce-backend/src/main/java/com/app/ecommerce/models/OrderItem.java
@@ -0,0 +1,33 @@
+package com.app.ecommerce.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Builder
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    private Integer quantity;
+
+    private BigDecimal price;
+}
