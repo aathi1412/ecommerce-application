dd75f51b4ee75dbfa93f14fde29e5deff4d70c4b
Aathi <aathithirunav@gmail.com>
2026-07-20 11:28:11 +0530

    feat(cartitemcontroller): Add cart item endpoints
    - Added GET endpoint to retrieve all cart items
    - Added POST endpoint to add item to cart
    - Added PUT endpoint to update cart item quantity
    - Added DELETE endpoint to delete cart item

--- ecommerce-backend/src/main/java/com/app/ecommerce/controller/CartItemController.java
+++ ecommerce-backend/src/main/java/com/app/ecommerce/controller/CartItemController.java
@@ -3,10 +3,14 @@ package com.app.ecommerce.controller;
 
 import com.app.ecommerce.dto.ApiResponse;
 import com.app.ecommerce.dto.cart.CartItemRequest;
+import com.app.ecommerce.dto.cart.CartItemResponse;
+import com.app.ecommerce.models.CartItem;
 import com.app.ecommerce.service.CartItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/cart")
@@ -15,18 +19,34 @@ public class CartItemController {
 
     private final CartItemService cartItemService;
 
-    @PostMapping("/items/{userId}")
-    public ResponseEntity<ApiResponse> addToCart(@PathVariable Long userId,
+    @GetMapping
+    public ResponseEntity<List<CartItemResponse>> getAllCartItems(@RequestParam Long userId){
+        List<CartItemResponse> cartItems = cartItemService.getAllCartItems(userId);
+        return ResponseEntity
+                .ok(cartItems);
+    }
+
+    @PostMapping
+    public ResponseEntity<ApiResponse> addToCart(@RequestParam Long userId,
                                           @RequestBody CartItemRequest request) {
         ApiResponse response = cartItemService.addToCart(userId, request);
         return  ResponseEntity
                 .ok(response);
     }
 
-    @PutMapping("/items/{userId}")
-    public ResponseEntity<ApiResponse> updateCartQuantity(@PathVariable Long userId,
+    @PutMapping("/items")
+    public ResponseEntity<ApiResponse> updateCartQuantity(@RequestParam Long userId,
                                           @RequestBody CartItemRequest request) {
         ApiResponse response = cartItemService.updateCartItemQuantity(userId, request);
+        return  ResponseEntity
+                .ok(response);
+    }
+
+    @DeleteMapping("/items")
+    public ResponseEntity<ApiResponse> deleteCartItem(@RequestParam Long userId,
+                                                      @RequestParam Long productId) {
+        ApiResponse response = cartItemService.deleteCartItem(userId, productId);
+
         return  ResponseEntity
                 .ok(response);
     }
