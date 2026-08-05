ffdfbe20c8c5adea2025dc1c4c453b72558e31e0
Aathi <aathithirunav@gmail.com>
2026-08-05 14:55:58 +0530

    refactor(userservice): Improve User Service
    - Changed getUser method to use id instead of email
    - Updated createUser method to return UserResponse
    - Modified updateUser method to use id and UpdateUserRequest

--- ecommerce-backend/src/main/java/com/app/ecommerce/service/UserService.java
+++ ecommerce-backend/src/main/java/com/app/ecommerce/service/UserService.java
@@ -1,8 +1,6 @@
 package com.app.ecommerce.service;
 
-import com.app.ecommerce.dto.user.AddressDTO;
-import com.app.ecommerce.dto.user.UserRequest;
-import com.app.ecommerce.dto.user.UserResponse;
+import com.app.ecommerce.dto.user.*;
 import com.app.ecommerce.exceptions.UserNotFoundException;
 import com.app.ecommerce.models.Address;
 import com.app.ecommerce.models.User;
@@ -29,22 +27,21 @@ public class UserService {
                 .collect(Collectors.toList());
     }
 
-    public UserResponse getUser(String email){
-        User user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new UserNotFoundException("User Not Found"));
+    public UserResponse getUserResponse(Long id){
+        User user = getUserById(id);
 
         return mapUserToUserResponse(user);
     }
 
-    public String createUser(UserRequest request){
+    public UserResponse createUser(CreateUserRequest request){
         User user = mapRequestToUser(request);
-        userRepository.save(user);
-        return "User Created  Successfully";
+        User savedUser = userRepository.save(user);
+        return mapUserToUserResponse(savedUser);
     }
 
     @Transactional
-    public String  updateUser(UserRequest request){
-        User user = userRepository.findByEmail(request.email())
+    public UserResponse  updateUser(Long id, UpdateUserRequest request){
+        User user = userRepository.findById(id)
                 .orElseThrow(() -> new UserNotFoundException("User Not Found"));
 
         user.setFirstName(request.firstName());
@@ -52,16 +49,16 @@ public class UserService {
         user.setPhone(request.phone());
 
         Address address = user.getAddress();
-        address.setStreet(request.address().getStreet());
-        address.setCity(request.address().getCity());
-        address.setState(request.address().getState());
-        address.setCountry(request.address().getCountry());
-        address.setZipcode(request.address().getZipcode());
+        address.setStreet(request.address().street());
+        address.setCity(request.address().city());
+        address.setState(request.address().state());
+        address.setCountry(request.address().country());
+        address.setZipcode(request.address().zipcode());
 
-        return "User Updated Successfully";
+        return mapUserToUserResponse(user);
     }
 
-    public User getUser(Long userId) {
+    public User getUserById(Long userId) {
         return userRepository.findById(userId)
                 .orElseThrow(() -> {
                     log.warn("User not found for id {} while updating cart", userId);
@@ -88,7 +85,7 @@ public class UserService {
     }
 
 
-    public User mapRequestToUser(UserRequest request){
+    public User mapRequestToUser(CreateUserRequest request){
         User user = User.builder()
                 .firstName(request.firstName())
                 .lastName(request.lastName())
@@ -98,11 +95,11 @@ public class UserService {
                 .build();
 
         Address address = Address.builder()
-                    .street(request.address().getStreet())
-                    .city(request.address().getCity())
-                    .state(request.address().getState())
-                    .country(request.address().getCountry())
-                    .zipcode(request.address().getZipcode())
+                    .street(request.address().street())
+                    .city(request.address().city())
+                    .state(request.address().state())
+                    .country(request.address().country())
+                    .zipcode(request.address().zipcode())
                     .build();
 
         user.setAddress(address);
